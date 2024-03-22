d43a4a1cd8421c32c2ed7d3091df6d6ee6e0d662
makona-OG <makonapharel@gmail.com>
2024-03-22 12:40:10 +0300

    recap

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,4 @@
-3
-rts.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
+#!/usr/bin/node
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) theFunction();
 };
